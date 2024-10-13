e807ff5b484eb501b3c94bdd58f8bc48cd3368bc
Alice Yuan <aliyuan@g.hmc.edu>
2024-10-13 15:47:27 -0700

    4th commit

--- output-parsers.js
+++ output-parsers.js
@@ -1,6 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers"
+import { StringOutputParser, CommaSeparatedListOutputParser } from "@langchain/core/output_parsers";
+import { StructuredOutputParser } from 'langchain/output_parsers';
 
 // Import environment variables
 import * as dotenv from "dotenv";
@@ -26,5 +27,36 @@ return await chain.invoke({
 })
 }
 
-const response = await callStringParser()
+async function callListOutputParsers() {
+    const prompt = ChatPromptTemplate.fromTemplate(
+        'Provide 5 synonyms, seperated by commas, for the following word {word}'
+    )
+    const outputParser = new CommaSeparatedListOutputParser();
+    const chain = prompt.pipe(model).pipe(outputParser);
+    return await chain.invoke({
+        word: "happy",
+    });
+}
+
+//Structured output parsers
+async function callStructureParsers() {
+    const prompt = ChatPromptTemplate.fromTemplate(
+        `Extract information from the following phrase.
+        Format Instructions: {format_instructions} 
+        Phrase: {phrase}`
+    )
+    const outputParser = StructuredOutputParser.fromNameAndDescriptions({
+        name: "the name of the person",
+        age: "the age of the person",
+    })
+    const chain = prompt.pipe(model).pipe(outputParser);
+    return await chain.invoke({
+        phrase: "Max is 30 years old",
+        format_instructions: outputParser.getFormatInstructions(),
+    })
+}
+
+//const response = await callStringParser()
+//const response = await callListOutputParsers()
+const response = await callStructureParsers()
 console.log(response);
